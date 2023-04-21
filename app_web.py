3c428ce0579127ac0556045ebc0d3f35eb198046
nghiatn1609 <nghiatn0916@gmail.com>
2023-04-21 22:46:31 +0700

    Update code

--- app_web.py
+++ app_web.py
@@ -85,7 +85,7 @@
 
 
 # thử lần 3 
-from flask import Flask, render_template, request, redirect, url_for , jsonify
+from flask import Flask, render_template, request, redirect, url_for
 import yfinance as yf
 import pandas as pd
 import numpy as np
@@ -97,18 +97,18 @@ from bokeh.resources import CDN
 from bokeh.embed import file_html
 from bokeh.models import DatetimeTickFormatter
 from bokeh.layouts import column
-# import the relevant Keras modules
 from keras.models import Sequential
 from keras.layers import Activation, Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 from keras.models import load_model
+# from flask_socketio import SocketIO
 
 
 
 app = Flask(__name__,static_folder='./static')
-app.secret_key = 'secret_key'
-
+# app.secret_key = 'secret_key'
+# socketio = SocketIO(app)
 def build_model(inputs, output_size, neurons, activ_func="linear",
                 dropout=0.25, loss="mae", optimizer="adam"):
     model = Sequential()
@@ -131,12 +131,12 @@ def predict_1_day(LSTM_training_inputs,LSTM_training_outputs,LSTM_test_inputs,te
     bt_history = bt_model.fit(LSTM_training_inputs, 
                                 LSTM_training_outputs, 
                                 epochs=30, batch_size=1, verbose=2, shuffle=True)
-    # # Lưu trữ mô hình vào file
-    # bt_model.save('lstm_btc_model.h5')
+    # Lưu trữ mô hình vào file
+    bt_model.save('lstm_btc_model.h5')
 
-    # # Đóng gói mô hình và lưu trữ vào file
-    # with open('lstm_btc_model.pkl', 'wb') as f:
-    #     pickle.dump(bt_model, f)
+    # Đóng gói mô hình và lưu trữ vào file
+    with open('lstm_btc_model.pkl', 'wb') as f:
+        pickle.dump(bt_model, f)
     #Predict
     result_1_day = bt_model(LSTM_test_inputs)
     # đưa kết quả dự đoán về giá trị ban đầu của Bitcoin
@@ -165,12 +165,12 @@ def predict_5_day(LSTM_training_inputs,LSTM_test_inputs,training_set,test_set,wi
     # note: eth_history contains information on the training error per epoch
     bt_history = bt_model_5_day.fit(LSTM_training_inputs[:-pred_range], LSTM_training_outputs_5_day, 
                                 epochs=40, batch_size=1, verbose=2, shuffle=True)
-    # # Lưu trữ mô hình vào file
-    # bt_model_5_day.save('lstm_btc_model_5_day.h5')
+    # Lưu trữ mô hình vào file
+    bt_model_5_day.save('lstm_btc_model_5_day.h5')
 
-    # # Đóng gói mô hình và lưu trữ vào file
-    # with open('lstm_btc_model_5_day.pkl', 'wb') as f:
-    #     pickle.dump(bt_model_5_day, f)
+    # Đóng gói mô hình và lưu trữ vào file
+    with open('lstm_btc_model_5_day.pkl', 'wb') as f:
+        pickle.dump(bt_model_5_day, f)
     #Predict 5 day 
     result_5_day = bt_model_5_day(LSTM_test_inputs[:-pred_range])
     # đưa kết quả dự đoán về giá trị ban đầu của Bitcoin
@@ -182,14 +182,84 @@ def predict_5_day(LSTM_training_inputs,LSTM_test_inputs,training_set,test_set,wi
     predicted_prices_5 = np.array(predicted_prices_denormalized)
 
     return predicted_prices_5[-1:]
+def predict_10_day(LSTM_training_inputs,LSTM_test_inputs,training_set,test_set,window_len):
+    # random seed for reproducibility
+    np.random.seed(202)
+    # we'll try to predict the closing price for the next 5 days 
+    # change this value if you want to make longer/shorter prediction
+    pred_range = 10
+    # initialise model architecture
+    bt_model_10_day = build_model(LSTM_training_inputs, output_size=pred_range, neurons = 20)
+    # model output is next 5 prices normalised to 10th previous closing price
+    LSTM_training_outputs_10_day = []
+    for i in range(window_len, len(training_set['bt_Close'])-pred_range):
+        LSTM_training_outputs_10_day.append((training_set['bt_Close'][i:i+pred_range].values/
+                                    training_set['bt_Close'].values[i-window_len])-1)
+    LSTM_training_outputs_10_day = np.array(LSTM_training_outputs_10_day)
+    # train model on data
+    # note: eth_history contains information on the training error per epoch
+    bt_history = bt_model_10_day.fit(LSTM_training_inputs[:-pred_range], LSTM_training_outputs_10_day, 
+                                epochs=40, batch_size=1, verbose=2, shuffle=True)
+    # Lưu trữ mô hình vào file
+    bt_model_10_day.save('lstm_btc_model_10_day.h5')
+
+    # Đóng gói mô hình và lưu trữ vào file
+    with open('lstm_btc_model_10_day.pkl', 'wb') as f:
+        pickle.dump(bt_model_10_day, f)
+    #Predict 10 day 
+    result_10_day = bt_model_10_day(LSTM_test_inputs[:-pred_range])
+    # đưa kết quả dự đoán về giá trị ban đầu của Bitcoin
+    predicted_prices_denormalized = []
+    for i in range(len(result_10_day)):
+        denormalized_price = (result_10_day[i]+1) * test_set['bt_Close'].values[i] 
+        # print(test_set['bt_Close'].values[i])
+        predicted_prices_denormalized.append(denormalized_price)
+    predicted_prices_10 = np.array(predicted_prices_denormalized)
+
+    return predicted_prices_10[-1:]
+def predict_20_day(LSTM_training_inputs,LSTM_test_inputs,training_set,test_set,window_len):
+    # random seed for reproducibility
+    np.random.seed(202)
+    # we'll try to predict the closing price for the next 5 days 
+    # change this value if you want to make longer/shorter prediction
+    pred_range = 20
+    # initialise model architecture
+    bt_model_20_day = build_model(LSTM_training_inputs, output_size=pred_range, neurons = 20)
+    # model output is next 5 prices normalised to 10th previous closing price
+    LSTM_training_outputs_20_day = []
+    for i in range(window_len, len(training_set['bt_Close'])-pred_range):
+        LSTM_training_outputs_20_day.append((training_set['bt_Close'][i:i+pred_range].values/
+                                    training_set['bt_Close'].values[i-window_len])-1)
+    LSTM_training_outputs_20_day = np.array(LSTM_training_outputs_20_day)
+    # train model on data
+    # note: eth_history contains information on the training error per epoch
+    bt_history = bt_model_20_day.fit(LSTM_training_inputs[:-pred_range], LSTM_training_outputs_20_day, 
+                                epochs=40, batch_size=1, verbose=2, shuffle=True)
+    # Lưu trữ mô hình vào file
+    bt_model_20_day.save('lstm_btc_model_20_day.h5')
+
+    # Đóng gói mô hình và lưu trữ vào file
+    with open('lstm_btc_model_20_day.pkl', 'wb') as f:
+        pickle.dump(bt_model_20_day, f)
+    #Predict 20 day 
+    result_20_day = bt_model_20_day(LSTM_test_inputs[:-pred_range])
+    # đưa kết quả dự đoán về giá trị ban đầu của Bitcoin
+    predicted_prices_denormalized = []
+    for i in range(len(result_20_day)):
+        denormalized_price = (result_20_day[i]+1) * test_set['bt_Close'].values[i] 
+        # print(test_set['bt_Close'].values[i])
+        predicted_prices_denormalized.append(denormalized_price)
+    predicted_prices_20 = np.array(predicted_prices_denormalized)
+
+    return predicted_prices_20[-1:]
 def predict(data):
     #EDA
     data['Date'] = pd.to_datetime(data['Date'])
     data = data.dropna()
     data.columns =[data.columns[0]]+['bt_'+i for i in data.columns[1:]]
-    #Take data from 2017
+    #Take data from 2018
     market_info = data
-    market_info = market_info[market_info['Date']>='2017-01-01']
+    market_info = market_info[market_info['Date']>='2018-01-01']
     #Create new columns
     for coins in ['bt_']: 
         kwargs = { coins+'day_diff': lambda x: (x[coins+'Close']-x[coins+'Open'])/x[coins+'Open']}
@@ -239,8 +309,10 @@ def predict(data):
     # predict_1_day= predict_1_day(LSTM_training_inputs,LSTM_training_outputs,LSTM_test_inputs,test_set)
     # predict_5_day= predict_5_day(LSTM_training_inputs,LSTM_test_inputs,training_set,test_set,window_len)
     
-    # result = {"predict_1_day": predict_1_day(LSTM_training_inputs,LSTM_training_outputs,LSTM_test_inputs,test_set), "predict_5_day": predict_5_day(LSTM_training_inputs,LSTM_test_inputs,training_set,test_set,window_len)}
+    # result = {"predict_1_day": predict_1_day(LSTM_training_inputs,LSTM_training_outputs,LSTM_test_inputs,test_set), "predict_5_day": predict_5_day(LSTM_training_inputs,LSTM_test_inputs,training_set,test_set,window_len),
+    #           "predict_10_day": predict_10_day(LSTM_training_inputs,LSTM_test_inputs,training_set,test_set,window_len), "predict_20_day": predict_20_day(LSTM_training_inputs,LSTM_test_inputs,training_set,test_set,window_len)}
     
+    #Dự đoán 1 ngày
     # Tải mô hình đã được đóng gói
     with open(r'C:/Users/Admin/OneDrive/Máy tính/SP_23/Model_predict/Bitcoin_web/lstm_btc_model.pkl', 'rb') as f:
         model = pickle.load(f)
@@ -255,6 +327,7 @@ def predict(data):
         predicted_prices_denormalized.append(denormalized_price)
     predicted_prices_1 = np.array(predicted_prices_denormalized)
     
+    #Dự đoán 5 ngày
     # Tải mô hình đã được đóng gói
     with open(r'C:/Users/Admin/OneDrive/Máy tính/SP_23/Model_predict/Bitcoin_web/lstm_btc_model_5_day.pkl', 'rb') as f:
         model_5_day = pickle.load(f)
@@ -269,7 +342,37 @@ def predict(data):
         predicted_prices_denormalized.append(denormalized_price)
     predicted_prices_5 = np.array(predicted_prices_denormalized)
     
-    result = {"predict_1_day":predicted_prices_1, "predict_5_day": predicted_prices_5}
+    #Dự đoán 10 ngày
+    # Tải mô hình đã được đóng gói
+    with open(r'C:/Users/Admin/OneDrive/Máy tính/SP_23/Model_predict/Bitcoin_web/lstm_btc_model_10_day.pkl', 'rb') as f:
+        model_10_day = pickle.load(f)
+
+    # Dự đoán với tập dữ liệu mới
+    predictions_10_day = model_10_day.predict(LSTM_test_inputs[:-5])
+    
+    # đưa kết quả dự đoán về giá trị ban đầu của Bitcoin
+    predicted_prices_denormalized = []
+    for i in range(len(predictions_10_day)):
+        denormalized_price = (predictions_10_day[i]+1) * test_set['bt_Close'].values[i] 
+        predicted_prices_denormalized.append(denormalized_price)
+    predicted_prices_10 = np.array(predicted_prices_denormalized)
+    
+    #Dự đoán 20 ngày
+    # Tải mô hình đã được đóng gói
+    with open(r'C:/Users/Admin/OneDrive/Máy tính/SP_23/Model_predict/Bitcoin_web/lstm_btc_model_20_day.pkl', 'rb') as f:
+        model_20_day = pickle.load(f)
+
+    # Dự đoán với tập dữ liệu mới
+    predictions_20_day = model_20_day.predict(LSTM_test_inputs[:-5])
+    
+    # đưa kết quả dự đoán về giá trị ban đầu của Bitcoin
+    predicted_prices_denormalized = []
+    for i in range(len(predictions_20_day)):
+        denormalized_price = (predictions_20_day[i]+1) * test_set['bt_Close'].values[i] 
+        predicted_prices_denormalized.append(denormalized_price)
+    predicted_prices_20 = np.array(predicted_prices_denormalized)
+    
+    result = {"predict_1_day":predicted_prices_1[-1:], "predict_5_day": predicted_prices_5[-1:], "predict_10_day": predicted_prices_10[-1:], "predict_20_day": predicted_prices_20[-1:]}
     
     return result
     
@@ -312,10 +415,10 @@ def descripton(bitcoin_name):
     description = {
         "description": coin_info[0]["description"],
         "logo": coin_info[0]["logo"],
-        "message_board": coin_info[0]["urls"]["message_board"][0],
+        "message_board": coin_info[0]["urls"]["message_board"], # có thể bỏ [0]
         "reddit": coin_info[0]["urls"]["reddit"][0],
         "source_code": coin_info[0]["urls"]["source_code"][0],
-        "technical_doc": coin_info[0]["urls"]["technical_doc"][0],
+        "technical_doc": coin_info[0]["urls"]["technical_doc"], # có thể bỏ [0]
         "website": coin_info[0]["urls"]["website"][0],
         
         }
@@ -389,14 +492,16 @@ def show_results(bitcoin_name):
     
     # Lấy ngày hiện tại
     today = datetime.datetime.today()
-    next_date = []
+    
+    #Hiển thị giá của 5 ngày tiếp theo
+    next_date_5 = []
     # Hiển thị date của 5 ngày tiếp theo
     for i in range(0, 5):
         next_day = today + datetime.timedelta(days=i)
-        next_date.append(next_day.date())
+        next_date_5.append(next_day.date())
     
     # Tạo dataframe với 2 cột là 'date' và 'price'
-    df_5_day = pd.DataFrame({'date': next_date, 'price': result_predict['predict_5_day'][0]})
+    df_5_day = pd.DataFrame({'date': next_date_5, 'price': result_predict['predict_5_day'][0]})
     df_5_day_html = df_5_day.to_dict('records')
     # In ra dataframe
     # print(df_5_day)
@@ -405,16 +510,61 @@ def show_results(bitcoin_name):
     predict_5 = figure(title="Bitcoin Price Predict",height=350, sizing_mode='stretch_width')
     predict_5.line(df_5_day['date'], df_5_day['price'], line_width=2)
 
-
     xformatter = DatetimeTickFormatter(days="%Y-%m-%d")
     predict_5.xaxis.formatter = xformatter
     # # generate the HTML code for the plot
-    plot_html = file_html(predict_5, CDN)
+    plot_html_5 = file_html(predict_5, CDN)
+    
+    #Hiển thị giá của 10 ngày tiếp theo 
+    next_date_10 = []
+    # Hiển thị date của 10 ngày tiếp theo
+    for i in range(0, 10):
+        next_day = today + datetime.timedelta(days=i)
+        next_date_10.append(next_day.date())
+    
+    # Tạo dataframe với 2 cột là 'date' và 'price'
+    df_10_day = pd.DataFrame({'date': next_date_10, 'price': result_predict['predict_10_day'][0]})
+    df_10_day_html = df_10_day.to_dict('records')
+
+    # create a figure with Bokeh
+    predict_10 = figure(title="Bitcoin Price Predict",height=350, sizing_mode='stretch_width')
+    predict_10.line(df_10_day['date'], df_10_day['price'], line_width=2)
+
+    xformatter = DatetimeTickFormatter(days="%Y-%m-%d")
+    predict_10.xaxis.formatter = xformatter
+    # # generate the HTML code for the plot
+    plot_html_10 = file_html(predict_10, CDN)
+    
+    #Hiển thị giá của 20 ngày tiếp theo
+    next_date_20 = []
+    # Hiển thị date của 5 ngày tiếp theo
+    for i in range(0, 20):
+        next_day = today + datetime.timedelta(days=i)
+        next_date_20.append(next_day.date())
+    
+    # Tạo dataframe với 2 cột là 'date' và 'price'
+    df_20_day = pd.DataFrame({'date': next_date_20, 'price': result_predict['predict_20_day'][0]})
+    df_20_day_html = df_20_day.to_dict('records')
+
+    # create a figure with Bokeh
+    predict_20 = figure(title="Bitcoin Price Predict",height=350, sizing_mode='stretch_width')
+    predict_20.line(df_20_day['date'], df_20_day['price'], line_width=2)
+
+    xformatter = DatetimeTickFormatter(days="%Y-%m-%d")
+    predict_20.xaxis.formatter = xformatter
+    # # generate the HTML code for the plot
+    plot_html_20 = file_html(predict_20, CDN)
     # Render template hiển thị kết quả
-    return render_template('./index.html', data1= data_list, data2= des ,data3 = result_predict, data4 = df_5_day_html, plot1 = dashboard(bitcoin_name), plot2 = plot_html)
+    return render_template('./index.html', data1= data_list, data2= des ,data3 = result_predict, data4 = df_5_day_html, data5 = df_10_day_html, data6 = df_20_day_html, plot1 = dashboard(bitcoin_name), plot2 = plot_html_5, plot3 = plot_html_10, plot4 = plot_html_20)
 
 # @app.route('/dashboard')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+# @socketio.on('connect')
+# def test_connect():
+#     print('Client connected')
+
+# if __name__ == '__main__':
+#     socketio.run(app)
